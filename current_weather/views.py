27880970f6192ecb180a5e10bda2559639eb1b6c
MilanAljetic <aljetic23@gmail.com>
2020-05-09 12:47:38 +0200

    Commit 2
    delete option, non duplicate option and just real city option.

--- current_weather/views.py
+++ current_weather/views.py
@@ -1,15 +1,44 @@
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import City
 from .forms import CityForm
 # Create your views here.
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=c12a48c9ac1ec37bedd3cc51b1af4e8b'
     
+    err_msg = ''
+    message = ''
+    message_class = ''
+
 
     if request.method == 'POST':
         form = CityForm(request.POST)
-        form.save()
+
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            existing_city_count = City.objects.filter(name=new_city).count()
+           
+            if existing_city_count == 0:
+                r = requests.get(url.format(new_city)).json()
+                
+                if r['cod'] == 200:
+                    form.save()
+                
+                else:
+                    err_msg = 'City does not exist in the world!'
+            else:
+                err_msg = 'City already exists in the database!'
+            #da ne budu 2 ista grada u listi i da ne bude grad koji ne postoji
+
+        if err_msg:
+           message = err_msg
+           message_class = 'is-danger'
+        else:
+            message =  'City added successfully'
+            message_class = 'is-success'
+
+
+    print(err_msg)
 
     form = CityForm()
 
@@ -35,8 +64,16 @@ def index(request):
 
     
 
-    context = {'weather_data' : weather_data, 'form' : form}
+    context = {
+        'weather_data' : weather_data, 
+        'form' : form,
+        'message' : message,
+        'message_class' : message_class
+    }
 
     return render(request, 'weather/weather.html',context)
 
+def delete_city(request, city_name):
+    City.objects.get(name=city_name).delete()
+    return redirect('home')
 
